956a1c44c2377f36c5157cc44de9bea1d06d9959
Mrzillka <mrzillka@gmail.com>
2024-06-17 14:50:46 +0700

    GitHub init

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,12 @@
-def get_mask_card_number(card_number: int | str) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Return a mask of 16-simbols card number in following format XXXX XX** **** XXXX."""
-    try:
-        card_number = str(card_number)
-    finally:
-        card_number = card_number.replace(" ", "")
+    card_number = str(card_number)
+    card_number = card_number.replace(" ", "")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
-def get_mask_account(account_number: int | str) -> str:
+def get_mask_account(account_number: str) -> str:
     """Return a mask of 20-simbols number in following format **XXXX."""
-    try:
-        account_number = str(account_number)
-    finally:
-        account_number = account_number.replace(" ", "")
+    account_number = str(account_number)
+    account_number = account_number.replace(" ", "")
     return f"**{account_number[-4:]}"
