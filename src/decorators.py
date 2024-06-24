6e91fad19c999796c8182f77d34d324b4b98586e
Mrzillka <mrzillka@gmail.com>
2024-06-25 04:34:10 +0700

    add decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+from functools import wraps
+from typing import Optional, Any
+
+
+def log(filename: Optional[str] = None) -> Any | None:
+    """Logging if function working successfully or not in the file (or in console by default)"""
+    def wrapper(funk):
+        @wraps(funk)
+        def inner(*args, **kwargs):
+            result = None
+            error = None
+            try:
+                result = funk(*args, **kwargs)
+                log_text = f"{funk.__name__} ok\n"
+            except Exception as e:
+                error = e.__class__
+                log_text = f"{funk.__name__} error: {e.__class__.__name__}. Inputs {args, kwargs}\n"
+            if filename:
+                with open(filename, 'a') as log_file:
+                    log_file.write(log_text)
+            else:
+                print(log_text)
+            if error:
+                raise error
+            return result
+
+        return inner
+
+    return wrapper
