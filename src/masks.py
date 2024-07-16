c4d6a5948d03ebdfb1a10d03a6d7a9ebd708548a
Mrzillka <mrzillka@gmail.com>
2024-07-17 03:02:45 +0700

    add logger to `utils/utils`

--- src/masks.py
+++ src/masks.py
@@ -12,13 +12,13 @@ logger.info("Using module masks")
 
 def get_mask_card_number(card_number: str) -> str:
     """Return a mask of 16-simbols card number in following format XXXX XX** **** XXXX."""
-    logger.info("Function 'get_mask_card_number' was called")
+    logger.debug("Function 'get_mask_card_number' was called")
     card_number = card_number.replace(" ", "")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Return a mask of 20-simbols number in following format **XXXX."""
-    logger.info("Function 'get_mask_account' was called")
+    logger.debug("Function 'get_mask_account' was called")
     account_number = account_number.replace(" ", "")
     return f"**{account_number[-4:]}"
