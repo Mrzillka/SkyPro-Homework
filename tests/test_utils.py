bf3c9213d40357967886f7b28c0100b705fd3d61
Mrzillka <mrzillka@gmail.com>
2024-07-20 02:12:41 +0700

    Separate get_transactions_from_json into separate functions.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,11 @@
 import json
 import os
 
-from utils.utils import get_transactions_from_json
+from utils.utils import get_transactions
 
 
 def test_get_transactions_from_json(transactions):
     with open("tr.json", "w") as f:
         json.dump(transactions, f)
-    assert get_transactions_from_json("tr.json") == transactions
+    assert get_transactions("tr.json") == transactions
     os.remove("tr.json")
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import _io
 import json
 import logging
 from typing import Any
@@ -12,15 +13,45 @@ logger = logging.getLogger(__name__)
 logger.info(f"Using module utils")
 
 
-def get_transactions_from_json(path: str) -> Any:
-    """Read all transactions from json file and return dict"""
-    logger.debug("Function 'get_mask_card_number' was called")
+def get_transactions(path: str) -> Any:
+    """Read all transactions from file and return dict"""
+    logger.debug("Function 'get_transactions' was called")
+    file_format = path[path.rfind('.'):]
     try:
         logger.info(f"Try to open json file {path}")
-        with open(path, encoding='utf-8') as f:
-            logger.info("Try to load json file")
-            transactions_list = json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        logger.warning("File not found or can't be converted from json")
+        with open(path) as f:
+            logger.info("File was opened")
+            if file_format == ".json":
+                transactions_list = get_transactions_from_json(f)
+            elif file_format == ".csv":
+                transactions_list = get_transactions_from_csv(f)
+            elif file_format == ".xlsx":
+                transactions_list = get_transactions_from_excel(f)
+            else:
+                logger.warning(f"Unsupported file format {file_format}")
+                transactions_list = []
+    except FileNotFoundError:
+        logger.warning(f"File {path} not found")
         return []
     return transactions_list
+
+
+def get_transactions_from_json(json_obj: _io.TextIOWrapper) -> Any:
+    """Load transactions from json file"""
+    try:
+        logger.info("Try to load json file")
+        transactions_list = json.load(json_obj)
+    except json.JSONDecodeError:
+        logger.warning("File can't be converted from json")
+        return []
+    return transactions_list
+
+
+def get_transactions_from_csv(csv_obj: _io.TextIOWrapper) -> Any:
+    """Load transactions from csv file"""
+    pass
+
+
+def get_transactions_from_excel(excel_obj: _io.TextIOWrapper) -> Any:
+    """Load transactions from exel file"""
+    pass
