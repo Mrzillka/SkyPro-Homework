d0f87b003fd3c758747181f8d364d59cad6a2e3d
Mrzillka <mrzillka@gmail.com>
2024-07-20 03:49:34 +0700

    Add get_transactions_from_excel function.

--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,14 @@
 import _io
+import csv
 import json
 import logging
-import csv
+import pandas as pd
 from typing import Any
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(module)s %(levelname)s: %(message)s',
-                    # filename=r'logs/utils_logs.log',
-                    filename=r'utils_logs.log',
+                    filename=r'logs/utils_logs.log',
+                    # filename=r'utils_logs.log',
                     filemode='w')
 
 logger = logging.getLogger(__name__)
@@ -20,18 +21,19 @@ def get_transactions(path: str) -> Any:
     logger.debug("Function 'get_transactions' was called")
     file_format = path[path.rfind('.'):]
     try:
-        logger.info(f"Try to open json file {path}")
-        with open(path, encoding="utf-8") as f:
-            logger.info("File was opened")
-            if file_format == ".json":
-                transactions_list = get_transactions_from_json(f)
-            elif file_format == ".csv":
-                transactions_list = get_transactions_from_csv(f)
-            elif file_format == ".xlsx":
-                transactions_list = get_transactions_from_excel(f)
-            else:
-                logger.warning(f"Unsupported file format {file_format}")
-                transactions_list = []
+        logger.info(f"Try to open file {path}")
+        if file_format == ".xlsx":
+            transactions_list = get_transactions_from_excel(path)
+        else:
+            with open(path, encoding="utf-8") as f:
+                logger.info("File was opened")
+                if file_format == ".json":
+                    transactions_list = get_transactions_from_json(f)
+                elif file_format == ".csv":
+                    transactions_list = get_transactions_from_csv(f)
+                else:
+                    logger.warning(f"Unsupported file format {file_format}")
+                    transactions_list = []
     except FileNotFoundError:
         logger.warning(f"File {path} not found")
         return []
@@ -58,6 +60,10 @@ def get_transactions_from_csv(csv_obj: _io.TextIOWrapper) -> Any:
     return transactions_list
 
 
-def get_transactions_from_excel(excel_obj: _io.TextIOWrapper) -> Any:
+def get_transactions_from_excel(path) -> Any:
     """Load transactions from exel file"""
-    pass
+    excel_obj = pd.read_excel(path)
+    return excel_obj.to_dict(orient="records")
+
+
+get_transactions_from_excel("transactions_excel.xlsx")
